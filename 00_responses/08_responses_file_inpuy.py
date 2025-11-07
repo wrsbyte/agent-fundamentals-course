69f64a9baaebcf29199d6aeb80e805b5bb21e7a0
wrsbyte <est.wilmerrodriguez@gmail.com>
2025-11-07 07:28:08 -0600

    Add initial implementation of response file input script

---
+++ 00_responses/08_responses_file_inpuy.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+import os
+
+
+def main():
+    print("🚀 Leer datos de un archivo...")
+
+    # Ver documentación en:
+    # https://platform.openai.com/docs/api-reference/responses/create?lang=python
+
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+if __name__ == "__main__":
+    main()
